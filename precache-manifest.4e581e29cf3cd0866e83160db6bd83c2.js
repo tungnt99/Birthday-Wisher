1b3836b9d528980cba7ad19dddbd88fc5e8b03fb
Tùng <92722248+tungnt99@users.noreply.github.com>
2023-02-16 16:24:55 +0700

    Updates

---
+++ precache-manifest.4e581e29cf3cd0866e83160db6bd83c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af49e62943e267a6995f241552f415ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4e51bfec44583767b60",
+    "url": "./static/css/main.0ad7cc55.chunk.css"
+  },
+  {
+    "revision": "704c1f0fa1ba0059b722",
+    "url": "./static/js/2.37e6f535.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.37e6f535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e51bfec44583767b60",
+    "url": "./static/js/main.4fd99279.chunk.js"
+  },
+  {
+    "revision": "ddb2488db294200d54e6",
+    "url": "./static/js/runtime-main.5517d39e.js"
+  }
+]);
